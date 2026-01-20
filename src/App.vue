b219197626477e9d6d7f75dd35ed98d286016483
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-20 11:23:46 +0100

    refactor(App): remove Extractor component and move everything directly to App comp

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts">
-import Extractor from "./components/Extractor.vue";
+import { ref } from "vue";
+import Debug from "./components/Debug.vue";
+import FileInput from "./components/FileInput.vue";
+import type { Lecture } from "./parser";
+
+const lectures = ref<Lecture[]>([]);
+
+const updateLectures = (newLectures: Lecture[]) => {
+    console.log("Updating lecutres", newLectures);
+    lectures.value = newLectures;
+};
 </script>
 
 <template>
-    <Extractor />
+    <div class="my-8">
+        <FileInput @lectures-loaded="updateLectures" />
+    </div>
+
+    <div>
+        <Debug :lectures />
+    </div>
 </template>
