8fb79a38b67fc87a2b2187c15fe67639fac75065
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-21 19:49:27 +0100

    feat(ExampleData): add an ExampleData component/button
    to use the example data instead of supplying the flex now html

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import Debug from "./components/Debug.vue";
 import Details from "./components/Details.vue";
 import FileInput from "./components/FileInput.vue";
 import type { Lecture } from "./parser";
+import ExampleData from "./components/ExampleData.vue";
 
 const lectures = ref<Lecture[]>([]);
 
@@ -17,6 +18,7 @@ const updateLectures = (newLectures: Lecture[]) => {
     <div class="m-8">
         <div class="my-8">
             <FileInput @lectures-loaded="updateLectures" />
+            <ExampleData @lectures-loaded="updateLectures" />
         </div>
 
         <div>
---
+++ src/components/ExampleData.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+/**
+ * Button to use example data if one wants to test the web app without supplying their own FlexNow data
+ *
+ * @emits lectures-loaded - propagates the `Lecture[]` list containing the example data - this needs to call a function that will replace the old lecture data with this new data
+ */
+export default {};
+</script>
+
+<script setup lang="ts">
+import rawExampleData from "@/assets/exampleData.json";
+import type { Lecture } from "@/parser";
+
+const exampleData = rawExampleData as Lecture[];
+
+const emit = defineEmits<{
+    (e: "lectures-loaded", data: Lecture[]): void;
+}>();
+
+function emitExampleData() {
+    emit("lectures-loaded", exampleData);
+}
+</script>
+
+<template>
+    <button @click="emitExampleData" class="btn">Use example data</button>
+</template>
