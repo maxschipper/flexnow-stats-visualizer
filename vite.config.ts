6ee1d13a51f145b9e2c8a1dc36dab182718eb753
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-21 19:39:55 +0100

    build(vite): change server settings like port and host

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+
+  server: {
+    host: true,
+    port: 5432,
+    strictPort: true,
+  },
 });
