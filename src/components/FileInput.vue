4807f97e18346e1b28d7868fd150646d0874bb14
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-21 19:37:03 +0100

    feat(FileInput): use daisyui file upload

--- src/components/FileInput.vue
+++ src/components/FileInput.vue
@@ -2,7 +2,7 @@
 /**
  * HTML file input to upload the file "`FlexNow Studentenschnittstelle.html`"
  *
- * @emits lectures-loaded - propagates the new `Lecture[]` list and needs to be handled in the parent component
+ * @emits lectures-loaded - propagates the new `Lecture[]` list  - this needs to call a function that will replace the old lecture data with this new data
  */
 export default {};
 </script>
@@ -38,8 +38,13 @@ const handleFileUpload = async (event: Event) => {
 
 <template>
     <div>
-        <h3>FlexNow Importer</h3>
-        <input type="file" @change="handleFileUpload" accept=".html" />
-        <div v-if="error" style="color: red">{{ error }}</div>
+        <!-- <h3>FlexNow Importer</h3>
+        <input type="file" @change="handleFileUpload" accept=".html" class="file-input" />
+        <div v-if="error" style="color: red">{{ error }}</div> -->
+        <fieldset class="fieldset">
+            <legend class="fieldset-legend">Upload your FlexNow HTML file</legend>
+            <input type="file" @change="handleFileUpload" accept=".html" class="file-input" />
+            <label class="label">it should be named FlexNow Studentenschnittstelle.html</label>
+        </fieldset>
     </div>
 </template>
