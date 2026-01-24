70eb6421122b3611725effacceeaba75c6136b7c
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-24 22:21:24 +0100

    feat(Details;Debug): make Details and Debug components collapsable

--- src/components/Debug.vue
+++ src/components/Debug.vue
@@ -13,11 +13,14 @@ defineProps<{ lectures: Lecture[] }>();
 </script>
 
 <template>
-    <div>
-        <div v-if="lectures.length === 0">Load lectures to show debug output</div>
+    <div v-if="lectures.length === 0">Load lectures to show debug output</div>
 
-        <div v-else>
-            <pre>{{ JSON.stringify(lectures, null, 2) }} </pre>
-        </div>
+    <div v-else class="collapse collapse-arrow">
+        <input type="checkbox" />
+        <!-- <div class="collapse-title font-semibold after:start-5 after:end-auto pe-4 ps-12">
+                    {{ lecture.id }}
+                </div> -->
+        <div class="collapse-title font-semibold after:start-5 after:end-auto pe-4 ps-12">Debug JSON</div>
+        <pre class="collapse-content text-xs">{{ JSON.stringify(lectures, null, 2) }} </pre>
     </div>
 </template>
--- src/components/Details.vue
+++ src/components/Details.vue
@@ -15,36 +15,38 @@ const isObject = (val: any) => val && typeof val === "object";
 </script>
 
 <template>
-    <div>
-        <div v-if="lectures.length === 0">Load a html file to show details</div>
+    <div v-if="lectures.length === 0">Load a html file to show details</div>
 
-        <div v-else>
-            <div v-for="lecture in lectures">
-                <div class="collapse collapse-arrow bg-base-100 border-base-300 border my-2">
-                    <input type="checkbox" />
-                    <div
-                        class="collapse-title font-semibold after:start-5 after:end-auto pe-4 ps-12"
-                    >
-                        {{ lecture.id }}
-                    </div>
-                    <div class="collapse-content text-sm">
-                        <h3 class="mb-2">Lecture Details</h3>
-                        <ul>
-                            <li v-for="(value, key) in lecture" :key="key">
-                                <template v-if="isObject(value)">
-                                    <strong>{{ key }}:</strong>
-                                    <ul>
-                                        <li v-for="(subValue, subKey) in value" :key="subKey">
-                                            {{ subKey }}: {{ subValue }}
-                                        </li>
-                                    </ul>
-                                </template>
+    <div v-else>
+        <div class="collapse collapse-arrow">
+            <input type="checkbox" />
+            <div class="collapse-title font-semibold after:start-5 after:end-auto pe-4 ps-12">Details</div>
+            <div class="collapse-content">
+                <div v-for="lecture in lectures">
+                    <div class="collapse collapse-arrow bg-base-100 border-base-300 border my-2">
+                        <input type="checkbox" />
+                        <div class="collapse-title font-semibold after:start-5 after:end-auto pe-4 ps-12">
+                            {{ lecture.id }}
+                        </div>
+                        <div class="collapse-content text-sm">
+                            <h3 class="mb-2">Lecture Details</h3>
+                            <ul>
+                                <li v-for="(value, key) in lecture" :key="key">
+                                    <template v-if="isObject(value)">
+                                        <strong>{{ key }}:</strong>
+                                        <ul>
+                                            <li v-for="(subValue, subKey) in value" :key="subKey">
+                                                {{ subKey }}: {{ subValue }}
+                                            </li>
+                                        </ul>
+                                    </template>
 
-                                <template v-else>
-                                    <strong>{{ key }}:</strong> {{ value }}
-                                </template>
-                            </li>
-                        </ul>
+                                    <template v-else>
+                                        <strong>{{ key }}:</strong> {{ value }}
+                                    </template>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
