fd16c2a948ddff1dbbc6e0f189fe9eaea5464c13
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-29 21:39:38 +0100

    fix(Scatter): fix scatter plot

--- src/components/Unovis/UnovisScatter.vue
+++ src/components/Unovis/UnovisScatter.vue
@@ -1,23 +1,26 @@
 <script setup lang="ts">
-import type { Lecture } from "@/parser";
 import { VisScatter, VisXYContainer, VisAxis } from "@unovis/vue";
 import { computed } from "vue";
+import { useLectures } from "@/composables/useLectures";
 
-const props = defineProps<{ lectures: Lecture[] }>();
+const { lectures } = useLectures();
 
 const mydata = computed(() =>
-    props.lectures.flatMap((lec) => {
-        if (!lec.stats.graded || !lec.stats.graded_but_not_passed) return [];
-        return [
-            {
-                id: lec.id,
-                title: lec.name,
-                difficulty: lec.stats.graded_but_not_passed / lec.stats.graded,
-                performance: lec.grade,
-                participants: lec.stats.participants,
-            },
-        ];
-    }),
+    lectures.value
+        .flatMap((lec) => {
+            if (lec.stats === null || lec.stats.graded === null || lec.stats.graded_but_not_passed === null)
+                return null;
+            return [
+                {
+                    id: lec.id,
+                    title: lec.name,
+                    difficulty: lec.stats.graded_but_not_passed / lec.stats.graded,
+                    performance: lec.grade,
+                    participants: lec.stats.participants,
+                },
+            ];
+        })
+        .filter((l) => l !== null),
 );
 
 const x = (d: any) => d.difficulty;
@@ -27,8 +30,8 @@ const size = (d: any) => d.participants;
 
 <template>
     <VisXYContainer :data="mydata">
-        <VisAxis type="y" :gridLine="false" label="My grade" :tickValues="[0, 1, 3, 4, 5]" />
-        <VisAxis type="x" :gridLine="false" label="Difficulty (failure rate)" />
+        <VisAxis type="y" :gridLine="true" label="My grade" :numTicks="5" :tickValues="[0, 1, 3, 4, 5]" />
+        <VisAxis type="x" :gridLine="false" :tickValues="[1, 2, 3]" label="Difficulty (failure rate)" />
         <VisScatter :x="x" :y="y" :size :sizeRange="[10, 50]" />
     </VisXYContainer>
 </template>
