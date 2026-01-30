79613d18d0c73f8bfed70889d9b53b9dd6c16baa
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-30 17:22:18 +0100

    feat(App): use ChartCard

--- src/App.vue
+++ src/App.vue
@@ -6,22 +6,28 @@ import RemoveData from "./components/RemoveData.vue";
 import ChartSankey from "./components/Chart/ChartSankey.vue";
 import ChartScatter from "./components/Chart/ChartScatter.vue";
 import { useLectures } from "./composables/useLectures";
+import ChartCard from "./components/Chart/ChartCard.vue";
 
 const { lectures } = useLectures();
 </script>
 
 <template>
     <Hero v-if="lectures.length === 0" />
-    <div v-else class="m-8">
+    <!-- <div v-else class="p-4"> -->
+    <div v-else class="">
+        <div>
+            <ChartCard heading="Performance vs. Difficulty Matrix">
+                <ChartScatter />
+            </ChartCard>
+            <!-- <ChartPerf /> -->
+            <ChartCard heading="Student Flow Chart">
+                <ChartSankey />
+            </ChartCard>
+        </div>
         <div>
             <RemoveData />
             <Details />
             <Debug />
-        </div>
-
-        <div>
-            <ChartScatter />
-            <ChartSankey />
         </div>
     </div>
 </template>
