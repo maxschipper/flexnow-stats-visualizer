f9a208a641753f95f903ab033dacb0a5151e14af
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-30 17:24:22 +0100

    feat(ChartCard): add card as a parent component for each chart

---
+++ src/components/Chart/ChartCard.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { heading = "missing heading prop" } = defineProps<{ heading: string }>();
+</script>
+
+<template>
+    <div class="m-4 p-3 bg-base-200 rounded-box shadow">
+        <h3 class="font-bold mb-4 ml-4">{{ heading }}</h3>
+        <slot />
+    </div>
+</template>
