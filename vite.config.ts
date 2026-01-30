343def08ca0570ef6548324d8334e363d6401018
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-30 15:53:56 +0100

    build: update port and add try cloudflare tunnel to allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -15,8 +15,8 @@ export default defineConfig({
 
   server: {
     host: true,
-    port: 5432,
+    port: 8080,
     strictPort: true,
-    allowedHosts: ["localhost", "127.0.0.1", "yoga", "yoga.nb.lab", "dev.nuc.lab"],
+    allowedHosts: ["localhost", "127.0.0.1", "yoga", "yoga.nb.cloud", "dev.nuc.lab", ".trycloudflare.com"],
   },
 });
