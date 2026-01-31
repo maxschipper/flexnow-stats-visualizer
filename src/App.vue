306b71d8d26db5388384811e3016abf83201a549
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-31 17:13:45 +0100

    feat(App): remove details, debug and reset button; now in navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
 import ChartSankey from "./components/Chart/ChartSankey.vue";
 import ChartScatter from "./components/Chart/ChartScatter.vue";
-import Debug from "./components/Debug.vue";
-import Details from "./components/Details.vue";
 import Hero from "./components/Hero.vue";
-import RemoveData from "./components/RemoveData.vue";
+import Navbar from "./components/Navbar.vue";
 import { useLectures } from "./composables/useLectures";
 
 const { lectures } = useLectures();
@@ -13,15 +11,13 @@ const { lectures } = useLectures();
 <template class="bg-base-100">
   <Hero v-if="lectures.length === 0" />
   <!-- <div v-else class="p-4"> -->
-  <div v-else class="container max-w-full px-2">
-    <div class="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
-      <ChartScatter />
-      <ChartSankey />
-    </div>
-    <RemoveData class="m-4" />
-    <div class="grid grid-cols-1 md:grid-cols-2">
-      <Details />
-      <Debug />
+  <div v-else>
+    <Navbar />
+    <div class="container max-w-full px-2">
+      <div class="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+        <ChartScatter class="mt-2" />
+        <ChartSankey />
+      </div>
     </div>
   </div>
 </template>
