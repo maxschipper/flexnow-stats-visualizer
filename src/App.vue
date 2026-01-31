d899a111c3ec43ed57c286518b7df826401d51e4
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-31 01:21:39 +0100

    feat(App): add grid layout

--- src/App.vue
+++ src/App.vue
@@ -10,16 +10,16 @@ import { useLectures } from "./composables/useLectures";
 const { lectures } = useLectures();
 </script>
 
-<template>
+<template class="bg-base-100">
     <Hero v-if="lectures.length === 0" />
     <!-- <div v-else class="p-4"> -->
-    <div v-else class="">
-        <div>
+    <div v-else class="container max-w-full px-2">
+        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
             <ChartScatter />
             <ChartSankey />
         </div>
-        <div>
-            <RemoveData />
+        <RemoveData class="m-4" />
+        <div class="grid grid-cols-1 md:grid-cols-2">
             <Details />
             <Debug />
         </div>
