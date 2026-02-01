0f6b197067ee33cdad283b04df4c3d77e035cc47
Max S <150921823+maxschipper@users.noreply.github.com>
2026-02-01 21:48:29 +0100

    feat(Debug;Details): make only one collapse open at a time

--- src/components/Debug.vue
+++ src/components/Debug.vue
@@ -13,11 +13,21 @@ const { lectures } = useLectures();
 
 <template>
   <div v-if="lectures.length === 0">Load lectures to show debug output</div>
-  <div v-else class="collapse-arrow bg-base-200 border-base-300 collapse">
+  <!-- <div v-else class="collapse-arrow bg-base-200 border-base-300 collapse">
     <input type="checkbox" />
     <div class="collapse-title ps-12 pe-4 font-semibold after:start-5 after:end-auto">
       Debug JSON
     </div>
     <pre class="collapse-content text-xs">{{ JSON.stringify(lectures, null, 2) }} </pre>
-  </div>
+  </div> -->
+  <details
+    v-else
+    name="accordion-group-1"
+    class="collapse-arrow bg-base-200 border-base-300 collapse"
+  >
+    <summary class="collapse-title ps-12 pe-4 font-semibold after:start-5 after:end-auto">
+      Debug JSON
+    </summary>
+    <pre class="collapse-content text-xs">{{ JSON.stringify(lectures, null, 2) }} </pre>
+  </details>
 </template>
--- src/components/Details.vue
+++ src/components/Details.vue
@@ -17,7 +17,7 @@ const { lectures } = useLectures();
 <template>
   <div v-if="lectures.length === 0">Load a html file to show details</div>
 
-  <div v-else>
+  <!-- <div v-else>
     <div class="collapse-arrow bg-base-200 collapse">
       <input type="checkbox" />
       <div class="collapse-title ps-12 pe-4 font-semibold after:start-5 after:end-auto">
@@ -33,7 +33,6 @@ const { lectures } = useLectures();
             <div class="collapse-content text-sm">
               <ul>
                 <li v-for="(value, key) in lecture" :key="key">
-                  <!-- <template v-if="isObject(value)"> -->
                   <template v-if="typeof value === 'object'">
                     <strong>{{ key }}:</strong>
                     <ul>
@@ -53,5 +52,42 @@ const { lectures } = useLectures();
         </div>
       </div>
     </div>
+  </div> -->
+  <div v-else>
+    <details
+      name="accordion-group-1"
+      class="collapse-arrow bg-base-200 border-base-100 collapse border"
+    >
+      <summary class="collapse-title ps-12 pe-4 font-semibold after:start-5 after:end-auto">
+        Details
+      </summary>
+      <div class="collapse-content">
+        <div v-for="lecture in lectures">
+          <details name="accordion-group-2" class="collapse-arrow bg-base-200 collapse">
+            <summary class="collapse-title ps-12 pe-4 font-semibold after:start-5 after:end-auto">
+              {{ lecture.id }}
+            </summary>
+            <div class="collapse-content text-sm">
+              <ul>
+                <li v-for="(value, key) in lecture" :key="key">
+                  <template v-if="typeof value === 'object'">
+                    <strong>{{ key }}:</strong>
+                    <ul>
+                      <li v-for="(subValue, subKey) in value" :key="subKey">
+                        {{ subKey }}: {{ subValue }}
+                      </li>
+                    </ul>
+                  </template>
+
+                  <template v-else>
+                    <strong>{{ key }}:</strong> {{ value }}
+                  </template>
+                </li>
+              </ul>
+            </div>
+          </details>
+        </div>
+      </div>
+    </details>
   </div>
 </template>
